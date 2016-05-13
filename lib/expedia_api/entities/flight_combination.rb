e1727e9b8b1aa999d4e50d420052224aacdb526a
fsainz <fsainz@gmail.com>
2016-05-13 19:14:40 +0200

    added piid, total_price, price_difference_to_cheapest_combination to FlightCombination

--- lib/expedia_api/entities/flight_combination.rb
+++ lib/expedia_api/entities/flight_combination.rb
@@ -1,11 +1,29 @@
 module ExpediaApi
   module Entities
     class FlightCombination
-      attr_accessor :outbound_leg, :return_leg
+      attr_accessor :outbound_leg, :return_leg, :cheapest_combination_price
       attr_reader :raw_data
 
       def initialize(raw_data)
         @raw_data = raw_data || {}
+      end
+
+      def piid
+        @raw_data[:PIID]
+      end
+
+      #returns a money object including the price. returns nil if there is no price
+      def total_price
+        if raw_data[:PriceInformation] && raw_data[:PriceInformation][:TotalPrice]
+          Money.new(raw_data[:PriceInformation][:TotalPrice][:Value].to_f * 100, raw_data[:PriceInformation][:TotalPrice][:CurrencyCode])
+        else
+          nil
+        end
+      end
+
+      def price_difference_to_cheapest_combination
+        return nil unless total_price && cheapest_combination_price
+        total_price - Money.new(cheapest_combination_price[:Value].to_f * 100, cheapest_combination_price[:CurrencyCode])
       end
 
     end
